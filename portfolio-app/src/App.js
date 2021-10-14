aaf6f31ada4bd90d3b1afeccaf03b534c6d9115a
Jordan Scott <jscott0751@gmail.com>
2021-10-14 06:00:37 -0400

    formatted all components

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePage from './Pages/HomePage/Homepage'
+
+
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
