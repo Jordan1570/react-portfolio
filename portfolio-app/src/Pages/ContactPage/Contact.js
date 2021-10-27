7dd5821599eb03317ca3e0c2865994cc79879b7c
Jordan Scott <jscott0751@gmail.com>
2021-10-27 02:28:11 -0400

    added text areas to contact page

--- portfolio-app/src/Pages/ContactPage/Contact.js
+++ portfolio-app/src/Pages/ContactPage/Contact.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
 
 export default function Contact() {
     return (
         <div>
-            
+            <h1>Contact</h1>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Email:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Message:
+                    <input type="text" name="name" />
+                </label>
+                <Button variant="dark">Submit</Button>
+            </form>
         </div>
     )
 }
