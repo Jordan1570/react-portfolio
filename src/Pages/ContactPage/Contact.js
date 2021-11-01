360b1dfba5daece2eb24e3d66740e8c7ce1874db
Jordan Scott <jscott0751@gmail.com>
2021-10-31 20:57:19 -0400

    init

--- src/Pages/ContactPage/Contact.js
+++ src/Pages/ContactPage/Contact.js
@@ -3,9 +3,9 @@ import { Button } from 'react-bootstrap';
 
 export default function Contact() {
     return (
-        <div style={{ textAlign: "center" }}>
+        <div>
             <h1>Contact</h1>
-            <form action="mailto:jscott0751@gmail.com?body=Body" method="GET" encType="text/plain" style={{ textAlign: "center" }}>
+            <form action="mailto:jscott0751@gmail.com?body=Body" method="GET" encType="text/plain">
                 <label style={{ display: "block" }}>
                     Name:
                     <textarea name="name" style={{ display: "block" }}></textarea>
@@ -19,8 +19,8 @@ export default function Contact() {
                     Message:
                     <textarea name="Body" style={{ display: "block" }}></textarea>
                 </label>
-                <input type="submit" name="submit"/>
-                <Button variant="dark" style={{ display: "block", textAlign: "center" }}>Submit</Button>
+                {/* <input type="submit" name="submit"/> */}
+                <Button variant="dark" style={{ display: "block", textAlign: "center", marginTop: "10px" }}>Submit</Button>
             </form>
         </div>
     )
